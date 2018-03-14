ccc9cfceae3639150419a7b38285be9015eb7bc7
YanYuHl <A@Thoxvi.com>
2018-03-14 13:33:36 +0800

    add Env class

---
+++ base/Env.cpp
@@ -0,0 +1,19 @@
+//
+// Created by yanyuhl on 18-3-14.
+//
+
+#include "Env.h"
+#include <mutex>
+
+class Env::Impl{
+public:
+
+};
+
+static std::mutex m;
+
+Env &Env::getInstance() {
+    std::lock_guard<std::mutex> locker(m);
+    static Env env;
+    return env;
+}
---
+++ base/Env.h
@@ -0,0 +1,28 @@
+//
+// Created by yanyuhl on 18-3-14.
+//
+
+#ifndef ROCKARBON_ENV_H
+#define ROCKARBON_ENV_H
+
+
+class Env {
+public:
+    static Env &getInstance();
+
+private:
+    Env();
+
+    ~Env();
+
+    Env(const Env &);
+
+    const Env &operator=(const Env &);
+
+    class Impl; // 为了封装实现 :)
+
+    Impl *mImpl;
+};
+
+
+#endif //ROCKARBON_ENV_H
