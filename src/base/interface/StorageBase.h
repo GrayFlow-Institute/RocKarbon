4fbb06ffd56b58b7089f15829974878e1cf6d486
YanYuHl <A@Thoxvi.com>
2018-03-16 09:13:47 +0800

    重制接口

--- src/base/interface/StorageBase.h
+++ src/base/interface/StorageBase.h
@@ -14,11 +14,11 @@ class StorageBase {
 
     virtual void init(std::string data)=0;
 
-    virtual std::string get(const std::string &key)=0;
+    virtual std::string get(long long key)=0;
 
-    virtual std::string deal(std::string key, std::string value)=0;
+    virtual std::string deal(long long key, std::string value)=0;
 
-    virtual int match(const std::string &key, const std::string &value)=0;
+    virtual int match(long long key, const std::string &value)=0;
 
     virtual void sync()=0;
 };
