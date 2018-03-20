ff508db4887acfa936fcaf91cf85f3b71832ceb6
YanYuHl <A@Thoxvi.com>
2018-03-20 12:52:21 +0800

    add libgcrypt package

--- src/base/security/HashGuard.cpp
+++ src/base/security/HashGuard.cpp
@@ -3,10 +3,23 @@
 //
 
 #include <mutex>
+#include <cstring>
+#include <string>
+
+#define CRYPTOPP_ENABLE_NAMESPACE_WEAK 1
+
+#include <cryptopp/md5.h>
+#include <cryptopp/sha.h>
+
 #include "HashGuard.h"
+
+
+using namespace CryptoPP;
+using namespace std;
 
 class HashGuard::Impl {
 public:
+
 };
 
 static std::mutex m;
@@ -19,12 +32,32 @@ HashGuard &HashGuard::getInstance() {
 
 const std::string HashGuard::getMD5(const std::string &data) {
 
+    byte digest[Weak::MD5::DIGESTSIZE];
 
-    return "";
+    Weak::MD5 hash;
+    hash.CalculateDigest(digest, (const byte *) data.c_str(), data.length());
+
+    string out;
+    char tmp[3];
+    for (auto i : digest) {
+        sprintf(tmp, "%02x", i);
+        out += tmp;
+    }
+    return out;
 }
 
 const std::string HashGuard::getSHA256(const std::string &data) {
-    return "";
+    byte digest[SHA256::DIGESTSIZE];
+
+    SHA256().CalculateDigest(digest, (const byte *) data.c_str(), data.length());
+
+    string out;
+    char tmp[3];
+    for (auto i : digest) {
+        sprintf(tmp, "%02x", i);
+        out += tmp;
+    }
+    return out;
 }
 
 HashGuard::HashGuard() = default;
--- test/security/test.cpp
+++ test/security/test.cpp
@@ -4,19 +4,26 @@
 
 
 #include <gtest/gtest.h>
+#include <string>
+#include "../../src/base/security/HashGuard.h"
 
-TEST(Security, MD5){
-    EXPECT_EQ("","");
+using namespace std;
+
+TEST(Security, MD5) {
+    string md5sum = "e19d5cd5af0378da05f63f891c7467af";
+    HashGuard &h = HashGuard::getInstance();
+    EXPECT_EQ(h.getMD5("abcd1234"), md5sum);
 }
 
 
-TEST(BasicTest, FailTest ){
-    EXPECT_EQ(false, false);
-}
+TEST(Security, SHA256) {
+    string sha256sum = "e9cee71ab932fde863338d08be4de9dfe39ea049bdafb342ce659ec5450b69ae";
+    HashGuard &h = HashGuard::getInstance();
+    EXPECT_EQ(h.getSHA256("abcd1234"), sha256sum);}
 
 
-TEST(BasicTest, EQTest){
-    EXPECT_EQ(6,6);
+TEST(BasicTest, EQTest) {
+    EXPECT_EQ(6, 6);
 }
 
 
