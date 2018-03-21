70ec53002bd66d332144a7bf51cf8405ff059911
YanYuHl <A@Thoxvi.com>
2018-03-21 16:29:08 +0800

    add network tests

--- src/main.cpp
+++ src/main.cpp
@@ -10,32 +10,5 @@
 using namespace std;
 
 int main(int argc, char **argv) {
-    Env &env = Env::getInstance();
-    LoggerBase* mainlog;
-    StringUtils utils;
 
-
-    env.putData(StringEnv::IP, "127.0.0.1");
-    env.putData(StringEnv::INSTANCE_ONLY_CODE, utils.getRandomString(32));
-    env.putData(StringEnv::SERVER_NAME, "测试服");
-    env.putData(StringEnv::SERVER_INFO, "测试信息");
-
-    env.putData(NumberEnv::PORT, 23333);
-    env.putData(NumberEnv::LOGGER, static_cast<int>(LoggerType::DEBUG));
-
-
-    mainlog=env.getLogger("main");
-
-    DiscService ds;
-    ds.init();
-
-    thread t([&ds] {
-        ds.run();
-    });
-
-    this_thread::sleep_for(std::chrono::milliseconds(1));
-    ds.down();
-
-    mainlog->debug("ds is down.now join.");
-    t.join();
 }
---
+++ test/network/test.cpp
@@ -0,0 +1,33 @@
+//
+// Created by yanyuhl on 18-3-21.
+//
+
+
+#include <gtest/gtest.h>
+#include "../../src/field/network/DiscService.h"
+#include "../../src/base/system/Env.h"
+
+using namespace std;
+
+TEST(Network, DiscService) {
+    Env &env = Env::getInstance();
+    DiscService ds;
+
+    EXPECT_EQ(ds.init(), false);
+
+    env.putData(StringEnv::IP, "127.0.0.1");
+    env.putData(StringEnv::INSTANCE_ONLY_CODE, "ABCD1234");
+    env.putData(StringEnv::SERVER_NAME, "测试服");
+    env.putData(StringEnv::SERVER_INFO, "测试信息");
+
+    env.putData(NumberEnv::PORT, 23333);
+    env.putData(NumberEnv::LOGGER, static_cast<int>(LoggerType::RELEASE));
+
+
+    EXPECT_EQ(ds.getStatus(), Status::NOTINIT);
+    EXPECT_EQ(ds.run(), false);
+    EXPECT_EQ(ds.down(), false);
+    EXPECT_EQ(ds.init(), true);
+    EXPECT_EQ(ds.getStatus(), Status::STOP);
+    EXPECT_EQ(ds.down(), false);
+}
