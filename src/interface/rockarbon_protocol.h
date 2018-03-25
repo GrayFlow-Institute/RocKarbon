67b70538aaa7617e0c881294b834fbfd7027904e
YanYuHl <A@Thoxvi.com>
2018-03-25 14:56:03 +0800

    修改愚蠢的语法错误

--- src/interface/rockarbon_protocol.h
+++ src/interface/rockarbon_protocol.h
@@ -5,14 +5,16 @@
 #ifndef ROCKARBON_PROTOCOL_H
 #define ROCKARBON_PROTOCOL_H
 
-namespace rockarbon::protocol {
-    // 协议字符串
-    const std::string PROT_HEAD = "rockarbon/1.0\n";
-    const std::string PROT_HEARBEAT_REQ = PROT_HEAD + "hearbeat request\n";
-    const std::string PROT_HEARBEAT_RES = PROT_HEAD + "hearbeat response\n";
-    const std::string PROT_SYNC_FIRST = PROT_HEAD + "sync first\n";
-    const std::string PROT_SYNC_MORE = PROT_HEAD + "sync more\n";
-    const std::string PROT_SYNC_DONE = PROT_HEAD + "sync done\n";
+namespace rockarbon {
+    namespace protocol {
+        // 协议字符串
+        const std::string PROT_HEAD = "rockarbon/1.0\n";
+        const std::string PROT_HEARBEAT_REQ = PROT_HEAD + "hearbeat request\n";
+        const std::string PROT_HEARBEAT_RES = PROT_HEAD + "hearbeat response\n";
+        const std::string PROT_SYNC_FIRST = PROT_HEAD + "sync first\n";
+        const std::string PROT_SYNC_MORE = PROT_HEAD + "sync more\n";
+        const std::string PROT_SYNC_DONE = PROT_HEAD + "sync done\n";
+    }
 }
 
 #endif //ROCKARBON_PROTOCOL_H
